6e3cd4b55d1646650e8bd1103503940b0cdc1abd
Andrei <andrei@andreiv.com>
2021-12-14 02:12:05 +0200

    first and probably last

---
+++ app.js
@@ -0,0 +1,44 @@
+var usersData = require("./utils/userStats");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+var express = require("express");
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const uri = process.env.NODE_APP_MONGODB;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+console.log("getData");
+getData();
+
+async function getData() {
+  let data = await usersData.getAllHoldersData();
+
+  console.log("connectDb");
+  client.connect(async (err) => {
+    console.log(err);
+    let snapshotname = Date.now().toString();
+
+    console.log("insertMany");
+    client
+      .db("stakeborgdao-explorer")
+      .collection("snapshot")
+      .insertOne({ snapshot: snapshotname, data: data })
+      .then(function () {
+        console.log("Data inserted"); // Success
+        process.exit();
+      })
+      .catch(function (error) {
+        console.log(error); // Failure
+      });
+  });
+}
+
+module.exports = app;
---
+++ utils/userStats.js
@@ -0,0 +1,407 @@
+var standard_token_abi = require("./../abi/standard_token.json");
+var governance_staking_abi = require("./../abi/governance_staking.json");
+var governance_rewards_abi = require("./../abi/governance_rewards.json");
+var yieldfarmtoken_bond_abi = require("./../abi/yieldfarmtoken_bond.json");
+var yieldfarmtoken_swingby_abi = require("./../abi/yieldfarmtoken_swingby.json");
+var yieldfarmtoken_xyz_abi = require("./../abi/yieldfarmtoken_xyz.json");
+var yieldfarmtoken_usdc_lp_abi = require("./../abi/yieldfarmtoken_usdc_lp.json");
+var yieldstaking_abi = require("./../abi/yieldstaking.json");
+var Web3 = require("web3");
+require("dotenv").config();
+
+const Moralis = require("moralis/node");
+
+Moralis.start({
+  serverUrl: process.env.REACT_APP_MORALIS_SERVER,
+  appId: process.env.REACT_APP_MORALIS_APP,
+  masterKey: process.env.REACT_APP_MORALIS_MASTER,
+});
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(process.env.REACT_APP_AWS_NODE)
+);
+
+const standard_contract_address = "0xda0c94c73d127ee191955fb46bacd7ff999b2bcd";
+
+const BOND_contract_address = "0x0391D2021f89DC339F60Fff84546EA23E337750f";
+const SWINGBY_contract_address = "0x8287c7b963b405b7b8d467db9d79eec40625b13a";
+const XYZ_contract_address = "0x618679df9efcd19694bb1daa8d00718eacfa2883";
+const USDC_SLP_contract_address = "0xf1e34d19f63b69eaa70952f2f64f735849959833";
+
+const governance_staking_dao_contract_adress =
+  "0xbA319F6F6AC8F45E556918A0C9ECDDE64335265C";
+const governance_rewards_contract_address =
+  "0x1fC8EfDb15FD5f9250077dD820C201B36bBc1f0B";
+
+const yield_staking_contract_address =
+  "0x7f4fe6776a9617847485d43db0d3a9b734e459c5";
+
+const yield_farm_bond_contract_address =
+  "0x2b31D07A2625a2fBAe68feed5a818ffc00dFB21b";
+
+const yield_farm_swingby_contract_address =
+  "0xab0a722e5e8e6ea4299fe0cbed7f62c2a904267a";
+
+const yield_farm_xyz_contract_address =
+  "0x2b89b42a95676dc74013ece6c07a760df5709c5c";
+
+const yield_farm_usdc_lp_contract_address =
+  "0x41099b337F8435579dea46C7840b730ca87Fd35A";
+
+let standard_contract;
+let governance_staking_contract;
+let governance_rewards_contract;
+let yield_staking_contract;
+let yield_unclaimed_bond_contract;
+let yield_unclaimed_swingby_contract;
+let yield_unclaimed_xyz_contract;
+let yield_unclaimed_usdc_lp_contract;
+
+async function init() {
+  standard_contract = new web3.eth.Contract(
+    standard_token_abi,
+    standard_contract_address
+  );
+  governance_staking_contract = new web3.eth.Contract(
+    governance_staking_abi,
+    governance_staking_dao_contract_adress
+  );
+  governance_rewards_contract = new web3.eth.Contract(
+    governance_rewards_abi,
+    governance_rewards_contract_address
+  );
+  yield_staking_contract = new web3.eth.Contract(
+    yieldstaking_abi,
+    yield_staking_contract_address
+  );
+  yield_unclaimed_bond_contract = new web3.eth.Contract(
+    yieldfarmtoken_bond_abi,
+    yield_farm_bond_contract_address
+  );
+  yield_unclaimed_swingby_contract = new web3.eth.Contract(
+    yieldfarmtoken_swingby_abi,
+    yield_farm_swingby_contract_address
+  );
+  yield_unclaimed_xyz_contract = new web3.eth.Contract(
+    yieldfarmtoken_xyz_abi,
+    yield_farm_xyz_contract_address
+  );
+  yield_unclaimed_usdc_lp_contract = new web3.eth.Contract(
+    yieldfarmtoken_usdc_lp_abi,
+    yield_farm_usdc_lp_contract_address
+  );
+}
+
+async function getWalletTokens(addr) {
+  if (!web3.utils.isAddress(addr)) return 0;
+  let wallet = 0;
+  wallet = await standard_contract.methods.balanceOf(addr).call();
+  return wallet;
+}
+
+async function getGovernanceStakedTokens(addr) {
+  if (!web3.utils.isAddress(addr)) return 0;
+  let govstake = 0;
+  govstake = await governance_staking_contract.methods.balanceOf(addr).call();
+  return govstake;
+}
+
+async function getGovernanceUnclaimedTokens(addr) {
+  if (!web3.utils.isAddress(addr)) return 0;
+  let pendingRewardsGov = 0;
+  const batch = new web3.BatchRequest();
+
+  let promise1 = new Promise((resolve, rej) => {
+    batch.add(
+      governance_rewards_contract.methods
+        .userMultiplier(addr)
+        .call.request(null, (err, res) => {
+          resolve(res);
+        })
+    );
+  });
+
+  let promise2 = new Promise((resolve, rej) => {
+    batch.add(
+      governance_rewards_contract.methods
+        .currentMultiplier()
+        .call.request(null, (err, res) => {
+          resolve(res);
+        })
+    );
+  });
+
+  let promise3 = new Promise((resolve, rej) => {
+    batch.add(
+      governance_staking_contract.methods
+        .balanceOf(addr)
+        .call.request(null, (err, res) => {
+          resolve(res);
+        })
+    );
+  });
+
+  batch.execute();
+
+  return Promise.all([promise1, promise2, promise3]).then((res) => {
+    const [userMultiplierGov, currentMultiplierGov, balance] = res;
+    let multiplier = currentMultiplierGov - userMultiplierGov;
+
+    pendingRewardsGov = balance * multiplier;
+
+    return pendingRewardsGov;
+  });
+}
+async function getFarmingUnclaimedTokensForYieldFarm(
+  addr,
+  currentEpoch,
+  yieldContract,
+  tokenContract,
+  db
+) {
+  if (!web3.utils.isAddress(addr)) return 0;
+  let pending_farm = 0;
+
+  let numberOfEpochs = await yieldContract.methods.numberOfEpochs.call().call();
+
+  let totalDistributedAmount =
+    await yieldContract.methods.totalDistributedAmount.call().call();
+
+  const batch = new web3.BatchRequest();
+  let promises = [];
+  for (let epoch = 1; epoch <= currentEpoch; epoch++) {
+    promises.push(
+      // eslint-disable-next-line no-loop-func
+      new Promise((resolve, rej) => {
+        batch.add(
+          yield_staking_contract.methods
+            .getEpochUserBalance(addr, tokenContract, epoch)
+            .call.request(null, (err, res) => {
+              resolve(res);
+            })
+        );
+      })
+    );
+  }
+  for (let epoch = 1; epoch <= currentEpoch; epoch++) {
+    promises.push(
+      // eslint-disable-next-line no-loop-func
+      new Promise((resolve, rej) => {
+        batch.add(
+          yield_staking_contract.methods
+            .getEpochPoolSize(tokenContract, epoch)
+            .call.request(null, (err, res) => {
+              resolve(res);
+            })
+        );
+      })
+    );
+  }
+
+  batch.execute();
+
+  await Promise.all(promises).then((res) => {
+    const firstBatch = res.slice(0, currentEpoch);
+    const secondBatch = res.slice(currentEpoch);
+
+    for (let i = 0; i < currentEpoch; i++) {
+      if (secondBatch[i] != 0) {
+        pending_farm +=
+          ((totalDistributedAmount / numberOfEpochs) * firstBatch[i]) /
+          secondBatch[i];
+      }
+    }
+  });
+
+  const harvested = Moralis.Object.extend(db);
+  const query = new Moralis.Query(harvested);
+  query.equalTo("user", addr.toLowerCase());
+  await query.find().then(function (events) {
+    if (events.length) {
+      events.forEach((event) => {
+        pending_farm -= event.attributes.totalValue;
+      });
+    }
+  });
+
+  if (pending_farm < 0) pending_farm = 0;
+
+  return pending_farm;
+}
+
+async function getFarmingUnclaimedTokens(addr) {
+  if (!web3.utils.isAddress(addr)) return 0;
+  let total_pending_farm = 0;
+
+  let currentEpoch = await await yield_staking_contract.methods
+    .getCurrentEpoch()
+    .call();
+
+  total_pending_farm += await getFarmingUnclaimedTokensForYieldFarm(
+    addr,
+    currentEpoch,
+    yield_unclaimed_bond_contract,
+    BOND_contract_address,
+    "YieldFarmBONDHarvest"
+  );
+  total_pending_farm += await getFarmingUnclaimedTokensForYieldFarm(
+    addr,
+    currentEpoch,
+    yield_unclaimed_swingby_contract,
+    SWINGBY_contract_address,
+    "YieldFarmSWINGBYHarvest"
+  );
+  total_pending_farm += await getFarmingUnclaimedTokensForYieldFarm(
+    addr,
+    currentEpoch,
+    yield_unclaimed_xyz_contract,
+    XYZ_contract_address,
+    "YieldFarmXYZHarvest"
+  );
+
+  total_pending_farm += await getFarmingUnclaimedTokensForYieldFarm(
+    addr,
+    currentEpoch,
+    yield_unclaimed_usdc_lp_contract,
+    USDC_SLP_contract_address,
+    "YieldFarmSLPUSDCHarvest"
+  );
+
+  return total_pending_farm;
+}
+
+async function getAirdopUnclaimedTokens(addr) {
+  return null;
+}
+
+async function getUserTokens(addr) {
+  await init();
+  let _addr = addr;
+  let _wallet = Number((await getWalletTokens(addr)) / 1000000000000000000);
+  let _governanceStaking = Number(
+    (await getGovernanceStakedTokens(addr)) / 1000000000000000000
+  );
+  let _governanceUnclaimed = Number(
+    (await getGovernanceUnclaimedTokens(addr)) /
+      1000000000000000000000000000000000000
+  );
+  let _farmingUnclaimed = Number(
+    (await getFarmingUnclaimedTokens(addr)) / 1000000000000000000
+  );
+  let _airdropUnclaimed = Number(
+    (await getAirdopUnclaimedTokens(addr)) / 1000000000000000000
+  );
+
+  let user = {
+    address: _addr,
+    wallet: _wallet.toFixed(2),
+    governanceStaking: _governanceStaking.toFixed(2),
+    governanceUnclaimed: _governanceUnclaimed.toFixed(2),
+    farmingUnclaimed: _farmingUnclaimed.toFixed(2),
+    airdropUnclaimed: _airdropUnclaimed.toFixed(2),
+    total: (
+      _wallet +
+      _governanceStaking +
+      _governanceUnclaimed +
+      _farmingUnclaimed +
+      _airdropUnclaimed
+    ).toFixed(2),
+  };
+  return user;
+}
+
+async function getAllHolders() {
+  let allUsers = [];
+
+  let dataOrigins = [
+    "YieldFarmBONDHarvest",
+    "YieldFarmSWINGBYHarvest",
+    "YieldFarmXYZHarvest",
+    "STANDARDTokenApproval",
+    "GovernanceStakingDeposit",
+    "YieldFarmStakingDeposit",
+    "YieldFarmSLPUSDCHarvest",
+  ];
+
+  let dataOriginColumn = [
+    "user",
+    "user",
+    "user",
+    "spender",
+    "user",
+    "user",
+    "user",
+  ];
+
+  let data, query, count, cnt;
+
+  for (let i = 0; i < dataOrigins.length; i++) {
+    data = Moralis.Object.extend(dataOrigins[i]);
+    query = new Moralis.Query(data);
+    count = await query.count();
+
+    cnt = 0;
+    while (cnt < count) {
+      await query
+        .skip(cnt)
+        .find()
+        .then(function (events) {
+          if (events.length) {
+            events.forEach((event) => {
+              if (
+                allUsers.indexOf(event.attributes[dataOriginColumn[i]]) === -1
+              ) {
+                allUsers.push(event.attributes[dataOriginColumn[i]]);
+              }
+            });
+          }
+        });
+
+      cnt += 100;
+    }
+  }
+  return allUsers;
+}
+
+async function getAllHoldersData() {
+  await init();
+  let data = [];
+  let holders = await getAllHolders();
+
+  holders = holders.filter(function (item) {
+    //this is a smart contract which we don't care about
+    return item !== "0xba319f6f6ac8f45e556918a0c9ecdde64335265c";
+  });
+
+  await Promise.all(
+    holders.map(async (holder) => {
+      let holderData = await getUserTokens(holder);
+      data.push({
+        address: holder,
+        wallet: holderData.wallet,
+        governanceStaking: holderData.governanceStaking,
+        governanceUnclaimed: holderData.governanceUnclaimed,
+        farmingUnclaimed: holderData.farmingUnclaimed,
+        airdropUnclaimed: holderData.airdropUnclaimed,
+        total: holderData.total,
+      });
+    })
+  );
+  return data;
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+class PendingPromise {
+  constructor(args) {
+    this.args = args;
+  }
+
+  execute() {
+    return new Promise(this.args);
+  }
+}
+
+exports.getAllHoldersData = getAllHoldersData;
